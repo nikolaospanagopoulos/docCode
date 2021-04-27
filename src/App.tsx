8aaa9060e7f48daf0dd69aa6b72ee41d64e80382
nikospan <nikos4222@outlook.com.gr>
2021-04-27 22:38:58 +0300

    ready for code editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import { useEffect, useState, useRef } from 'react';
 import * as esbuild from 'esbuild-wasm'
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-
+import { fetchPlugin } from './plugins/fetch-plugin'
 
 function App() {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
 
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: '/esbuild.wasm'
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
     })
 
   }
@@ -20,31 +20,57 @@ function App() {
     if (!ref.current) {
       return;
     }
+    iframe.current.srcdoc = html;
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
       write: false,
-      plugins: [unpkgPathPlugin()],
+      plugins: [
+        unpkgPathPlugin(),
+        fetchPlugin(input)
+      ],
       define: {
         'process.env.NODE_ENV': '"production"',
-        global:'window'
+        global: 'window'
       }
     })
-    setCode(result.outputFiles[0].text)
+    // setCode(result.outputFiles[0].text)
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*')
   }
   useEffect(() => {
     startService()
   }, [])
 
+  const html = `
+  <html>
+    <head></head>
+    <body>
+      <div id="root"></div>
+      <script>
+        window.addEventListener('message',(event) => {
+          try{
+            eval(event.data);
+          }catch(error){
+            const root = document.querySelector('#root');
+            root.innerHTML = '<div style="color:red;"><h4>Runtime Error:</h4>' + error + '</div>';
+            throw error;
+          }
+          
+        },false);
+      </script>
+    </body>
+  </html>
+  `;
   return (
     <div className="App">
       <textarea onChange={e => setInput(e.target.value)} value={input}></textarea>
       <div>
         <button onClick={() => onClickButton()}>Submit</button>
       </div>
-      <pre>{code}</pre>
+      <iframe ref={iframe} srcDoc={html} sandbox='allow-scripts' title='code-preview' />
     </div>
   );
 }
+
 
 export default App;
