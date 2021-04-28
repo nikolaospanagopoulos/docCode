762a8a59c1b475666885749b50c73b649e675668
nikospan <nikos4222@outlook.com.gr>
2021-04-29 02:32:00 +0300

    added markdown editor and css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import {CodeWrapper} from './components/code-wrapper'
+//import {CodeWrapper} from './components/code-wrapper'
 import 'bulmaswatch/superhero/bulmaswatch.min.css'
-
+import {TextEditor} from './components/text-editor'
 function App() {
 
 
   return (
     <div className="App">
-      <CodeWrapper/>
+     <TextEditor/>
     </div>
   );
 }
