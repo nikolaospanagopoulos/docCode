fecb8eef7385aabc0369f8c9c13f790e0b15d337
Nikolaos Panagopoulos <nikos4222@outlook.com.gr>
2021-05-01 18:26:31 +0300

    added bundles reducer

--- src/state/reducers/bundlesReducer.ts
+++ src/state/reducers/bundlesReducer.ts
@@ -1,1 +1,34 @@
-export {}
+import produce from 'immer'
+import { ActionType } from '../action-types/index'
+import { Action } from '../actions/index'
+
+interface BundlesState {
+    [key: string]: {
+        loading: boolean;
+        code: string;
+        err: string
+    }
+}
+
+const initialState: BundlesState = {};
+
+export const bundlesReducer = produce((state: BundlesState = initialState, action: Action): BundlesState => {
+    switch (action.type) {
+        case ActionType.BUNDLE_START:
+            state[action.payload.cellId] = {
+                loading: true,
+                code: '',
+                err: ''
+            }
+            return state;
+        case ActionType.BUNDLE_COMPLETE:
+            state[action.payload.cellId] = {
+                loading: false,
+                code: action.payload.bundle.code,
+                err: action.payload.bundle.error
+            }
+            return state;
+        default:
+            return state;
+    }
+})
--- src/state/reducers/index.ts
+++ src/state/reducers/index.ts
@@ -1,12 +1,14 @@
 import { cellsReducer } from './cellsReducer'
+import { bundlesReducer } from './bundlesReducer'
 import { combineReducers } from 'redux'
 
 const reducers = combineReducers({
-    cells:cellsReducer
+    cells: cellsReducer,
+    bundles: bundlesReducer
 })
 
 
-export {reducers}
+export { reducers }
 
 
 export type RootState = ReturnType<typeof reducers>;
