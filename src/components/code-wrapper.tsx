18ba82797e9aeb86b10b9061a8107289882d4fb1
Nikolaos Panagopoulos <nikos4222@outlook.com.gr>
2021-05-02 20:55:43 +0300

    added loading animation fix

--- src/components/code-wrapper.tsx
+++ src/components/code-wrapper.tsx
@@ -1,44 +1,63 @@
-import { useEffect } from 'react';
-import { CodeEditor } from './code-editor'
-import { Preview } from './preview'
-import { Resizable } from './resizable'
-import { Cell } from '../state/cell';
-import { useActions } from '../hooks/use-actions'
-import {useTypedSelector} from '../hooks/use-typed-selector'
+import { useEffect } from "react";
+import { CodeEditor } from "./code-editor";
+import { Preview } from "./preview";
+import { Resizable } from "./resizable";
+import { Cell } from "../state/cell";
+import { useActions } from "../hooks/use-actions";
+import { useTypedSelector } from "../hooks/use-typed-selector";
+import "./code-wrapper.css";
 interface CodeWrapperProps {
-    cell: Cell
+  cell: Cell;
 }
 const CodeWrapper: React.FC<CodeWrapperProps> = ({ cell }) => {
-    const { updateCell,createBundle } = useActions()
+  const { updateCell, createBundle } = useActions();
 
- const bundle = useTypedSelector((state) =>state.bundles[cell.id])
-    useEffect(() => {
-        const timer = setTimeout(async () => {
-            createBundle(cell.id,cell.content)
-        }, 1000);
+  const bundle = useTypedSelector((state) => state.bundles[cell.id]);
+  useEffect(() => {
+    if (!bundle) {
+      createBundle(cell.id, cell.content);
+      return;
+    }
+    const timer = setTimeout(async () => {
+      createBundle(cell.id, cell.content);
+    }, 1000);
 
-        return () => {
-            clearTimeout(timer)
-        }
-    }, [cell.content,cell.id,createBundle])
+    return () => {
+      clearTimeout(timer);
+    };
+    // eslint-disable-next-line
+  }, [cell.content, cell.id, createBundle]);
 
-
-
-
-
-
-    return (
-        <Resizable direction="vertical">
-            <div className="App" style={{ height: 'calc(100% - 10px)', display: 'flex', flexDirection: 'row' }}>
-                <Resizable direction='horizontal'>
-                    <CodeEditor initialValue={cell.content} onChange={(value) => updateCell(cell.id, value)} />
-                </Resizable>
-                {bundle && <Preview code={bundle.code} error={bundle.err} />}
-                
-            </div>
+  return (
+    <Resizable direction="vertical">
+      <div
+        className="App"
+        style={{
+          height: "calc(100% - 10px)",
+          display: "flex",
+          flexDirection: "row",
+        }}
+      >
+        <Resizable direction="horizontal">
+          <CodeEditor
+            initialValue={cell.content}
+            onChange={(value) => updateCell(cell.id, value)}
+          />
         </Resizable>
-    );
-}
-
+        <div className="progress-wrapper">
+          {!bundle || bundle.loading ? (
+            <div className="progress-cover">
+              <progress className="progress is-small is-primary" max="100">
+                Loading...
+              </progress>
+            </div>
+          ) : (
+            <Preview code={bundle.code} error={bundle.err} />
+          )}
+        </div>
+      </div>
+    </Resizable>
+  );
+};
 
 export { CodeWrapper };
